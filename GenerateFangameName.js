eae3b42e5a9d5b90a563ce31ea8a3d50e1126a23
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-23 12:59:26 +0100

    chatgpt :trollface:

--- GenerateFangameName.js
+++ GenerateFangameName.js
@@ -18,3 +18,14 @@ function generateName() {
         })
         .catch(error => console.error('Error reading surname.txt:', error));
 }
+function showPopup() {
+    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('loader').style.display = 'block';
+    document.getElementById('generatedName').textContent = '';
+
+    setTimeout(() => {
+        generateName();
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('generatedName').textContent = generateName();
+    }, 1500);
+}
